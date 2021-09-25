3362c93de9dbc5a51313b7efc76980e71f0961cb
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2021-09-26 02:49:36 +0300

    Создает поиск стран и оформление

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+import './css/styles.css';
+import refs from './js/refs';
+import API from './js/fetchCountries';
 
-const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+import allCountriesMarkup from './templates/allCountriesMarkup.hbs';
+import countryCardMarkup from './templates/countryCardMarkup.hbs';
 
-const r = fetch('https://restcountries.eu/rest/v2/name');
-console.log(r);
+import { debounce } from 'lodash';
+import { Notify, Loading } from 'notiflix';
+
+const { success, warning, failure, info } = Notify;
+
+refs.searchInput.addEventListener('input', debounce(onCountriesSearch, 1000));
 
 
+function onCountriesSearch(evt) {
+  API.fetchCountries(evt.target.value).then(setInputValidation).catch(fetchError)
+}
 
-// const URL = 'https://pokeapi.co/api/v2';
+function setInputValidation(country) {
+  if (country.status === 404) {
+    refs.countryCard.innerHTML = '';
+    serverResponse(failure, 'Такой страны не существует! Введите правильное название страны!');
+    return
+  } else if (country.length > 10) {
+    refs.countryCard.innerHTML = '';
+    serverResponse(warning, 'Найдено слишком много совпадений. Введите более конкретный запрос!');
+    return
+  } else if (country.length > 1) {
+    refs.countryCard.innerHTML = allCountriesMarkup(country);
+    serverResponse(info, 'Выберите страну из списка или введите полное название');
+    return
+  } else if (country.length === 1) {
+    refs.countryCard.innerHTML = countryCardMarkup(country);
+    serverResponse(success, 'Выполнено успешно!');
+    refs.searchInput.value = '';
+  }
+}
 
-// const r = fetch('https://pokeapi.co/api/v2')
-// console.log(r);
+function fetchError(err) {
+  alert({ text: 'Check the correctness of the data entered!' })
+}
+
+function serverResponse(r, message) {
+  r(message);
+  Loading.remove();
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  searchInput: document.querySelector('.js-input'),
+  countriesList: document.querySelector('.js-countries-list'),
+  countryCard: document.querySelector('.js-country'),
+}
